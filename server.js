f278ec23770e77755152b73b1a7efbb37c58a2bf
pHilip <Malaxor>
2017-12-28 17:03:29 -0700

    added back-end files and folders

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//===========================================================================
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -17,7 +18,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "mongodb://localhost/musicTYE",
   {
     useMongoClient: true
   }
